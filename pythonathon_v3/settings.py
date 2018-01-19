b6f4c9c1981298c5fc6653bd8c64ba2f6dd586ac
dmaxson <dmaxson@ltsnet.net>
2018-01-19 17:28:57 -0500

    Put DB back into postgres (why did it get switched back to SQLite?)

--- pythonathon_v3/settings.py
+++ pythonathon_v3/settings.py
@@ -97,18 +97,18 @@ WSGI_APPLICATION = 'pythonathon_v3.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'pythonathon',
-    #     'USER': 'postgres',
-    #     'PASSWORD': open(os.environ.get('PASSWORD_FILE', '/dev/null')).read().strip(),
-    #     'HOST': 'db',
-    #     'PORT': '',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': 'postgres',
+        'PASSWORD': open(os.environ.get('POSTGRES_PASSWORD_FILE', '/dev/null')).read().strip(),
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': '',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
